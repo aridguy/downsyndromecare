70694c06361a8243d2cdb99817904f519ccedc7b
aridguy <rotimiariyo@gmail.com>
2025-05-03 21:08:14 +0100

    modified the app

--- src/App.js
+++ src/App.js
@@ -16,8 +16,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn 
         </a>
+        <button className='btn '>hello</button>
       </header>
     </div>
   );
