e3d636266c59b2d4eda4771232bdb7be7b63f8c9
aridguy <rotimiariyo@gmail.com>
2026-05-24 17:14:33 +0100

    modified the donation page

--- src/routes/donation/Donation.js
+++ src/routes/donation/Donation.js
@@ -32,7 +32,7 @@ const Donation = () => {
   }, [clientSuccess]) // Empty dependency array means this runs once on component mount
 
   const Navigate = useNavigate()
-  const [amount, setAmount] = useState("0")
+  const [amount, setAmount] = useState("")
   const [currency, setCurrency] = useState('NGN')
   const [donorInfo, setDonorInfo] = useState({
     name: '',
